8e30c4668bdf430c48a1a8dd94b6beba27a09711
Hassan4 <hassan.ibrahim.90@gmail.com>
2017-03-18 02:36:35 +0200

    Update Servo.c

--- Servo.c
+++ Servo.c
@@ -11,16 +11,7 @@
 #include "inc/hw_ints.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
-#include "driverlib/debug.h"
-#include "driverlib/pwm.h"
-#include "driverlib/pin_map.h"
-#include "inc/hw_gpio.h"
-#include "driverlib/timer.h"
-#include "driverlib/interrupt.h"
-#include "driverlib/rom.h"
-#include "Servo.h"
-#include "Servo_private.h"
-#include "utils_functions.h"
+
 
 //////////global variables for timer interrupt and degrees of SW PWM Angles/////////////
 volatile uint8_t anglesArr[180];
